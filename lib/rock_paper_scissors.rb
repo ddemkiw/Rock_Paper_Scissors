d74fe55ef7bf0c2a754d63b69eb20dac76e42dd9
Danielle Demkiw <ddemkiw@gmail.com>
2015-01-10 12:14:13 +0000

    Adding auto_guess functionality and '/draw' page

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -11,10 +11,26 @@ class RockPaperScissors < Sinatra::Base
     erb :new_game
   end
 
-  post '/rock' do
-    p params[:rps]
+  post '/guess' do
+    @computer = auto_guess.to_s
+    p @computer
+    @guess =  params[:rps]
+    p @guess
+    if @guess == @computer
+      redirect '/draw'
+    end
     erb :new_game
   end
+
+  get '/draw' do 
+    erb :draw
+  end
+
+
+  def auto_guess
+    [:rock, :paper, :scissors].sample
+  end 
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
