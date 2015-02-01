16c9fe6f069a1016bb3c4d4f3322fd4efeffc082
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-01 20:05:16 +0000

    starting again

--- features/step_definitions/custom_steps.rb
+++
@@ -1,9 +0,0 @@
-Then /^I should see the image "(.+)"$/ do |image|
-    expect(page).to have_xpath("//img[contains(@src, \"#{image}\")]")
-end
-
-When /^I press "([^\"]*)" within "([^\"]*)"$/ do |button,scope_selector|
-  within(scope_selector) do      
-    click_button(button)
-  end
-end
